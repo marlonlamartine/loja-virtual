96509ab6d281989463fc51febe862465d52ad9ea
Marlon Lamartine <marlon.lamartine@gmail.com>
2020-09-15 13:32:57 -0300

    Primeira versão

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:loja_virtual/tabs/home_tab.dart';
+import 'package:loja_virtual/tabs/orders_tab.dart';
+import 'package:loja_virtual/tabs/places_tab.dart';
+import 'package:loja_virtual/tabs/products_tab.dart';
+import 'package:loja_virtual/widgets/cart_button.dart';
+import 'package:loja_virtual/widgets/custom_drawer.dart';
+
+class HomeScreen extends StatelessWidget {
+
+  final _pagecontroller = PageController();
+
+  @override
+  Widget build(BuildContext context) {
+    return PageView(
+      controller: _pagecontroller,
+      physics: NeverScrollableScrollPhysics(),
+      children: <Widget>[
+        Scaffold(
+          body: HomeTab(),
+          drawer: CustomDrawer(_pagecontroller),
+          floatingActionButton: CartButton(),
+        ),
+        Scaffold(
+          appBar: AppBar(
+            title: Text("Produtos"),
+            centerTitle: true,
+          ),
+          drawer: CustomDrawer(_pagecontroller),
+          body: ProductsTab(),
+          floatingActionButton: CartButton(),
+        ),
+        Scaffold(
+          appBar: AppBar(
+            title: Text("Lojas"),
+            centerTitle: true,
+          ),
+          body: PlacesTab(),
+          drawer: CustomDrawer(_pagecontroller),
+        ),
+        Scaffold(
+          appBar: AppBar(
+            title: Text("Meus Pedidos"),
+            centerTitle: true,
+          ),
+          body: OrdersTab(),
+          drawer: CustomDrawer(_pagecontroller),
+        )
+      ],
+    );
+  }
+}
